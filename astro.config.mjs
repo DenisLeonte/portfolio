2e6f1e108ab45e2b186cf4b8fe098602f42ce83a
Denis Leonte <denis.leonte123@gmail.com>
2026-02-21 18:24:13 +0200

    things change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 // ⚠️  Update `site` with your actual GitHub username before deploying!
 export default defineConfig({
-  site: 'https://DenisLeonte.github.io',
-  base: '/portfolio',
+  site: 'https://denistechs.com',
+  base: '/',
   output: 'static',
   integrations: [
     react(),
